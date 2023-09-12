fa41e7c579d2a9bcbb1a867b9196b9328358309f
Tom Fan <fanhoman123@gmail.com>
2023-09-12 23:30:16 +0800

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,51 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-const blogCollection = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      id: z.string(),
-      title: z.string(),
-      description: z.string(),
-      date: z.string(),
-      datetime: z.string(),
-      category: z.string(),
-      disable: z.boolean(),
-      imageUrl: image().refine((img) => img.width >= 600, {
-        message: "Image must be at least 600 px width.",
-      }),
-    }),
-});
-const showcaseCollection = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      id: z.string(),
-      title: z.string(),
-      description: z.string(),
-      date: z.string(),
-      datetime: z.string(),
-      category: z.string(),
-      disable: z.boolean(),
-      imageUrl: image().refine((img) => img.width >= 600, {
-        message: "Image must be at least 600 px width.",
-      }),
-    }),
-});
-const resourcesCollection = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      id: z.string(),
-      title: z.string(),
-      description: z.string(),
-      date: z.string(),
-      datetime: z.string(),
-      category: z.string(),
-      disable: z.boolean(),
-      imageUrl: image().refine((img) => img.width >= 600, {
-        message: "Image must be at least 600 px width.",
-      }),
-    }),
-});
-const documentationCollection = defineCollection({
+const collection = defineCollection({
   schema: ({ image }) =>
     z.object({
       id: z.string(),
@@ -62,8 +17,8 @@ const documentationCollection = defineCollection({
 });
 
 export const collections = {
-  blog: blogCollection,
-  showcase: showcaseCollection,
-  resources: resourcesCollection,
-  documentation: documentationCollection,
+  blog: collection,
+  showcase: collection,
+  resources: collection,
+  documentation: collection,
 };
