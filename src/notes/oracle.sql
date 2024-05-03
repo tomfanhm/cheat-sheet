349b24d432c95d539d8af46992b23de4145961d2
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2024-05-03 20:05:39 +0800

    update object

--- src/notes/oracle.sql
+++
@@ -1,7 +0,0 @@
-DECLARE
-    result          datatype;
-    object_variable type_name;
-BEGIN
-    object_variable := type_name(attribute1, attribute2);
-    result := object_variable.method_name(parameters);
-END;
